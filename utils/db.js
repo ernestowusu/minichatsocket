fd077da9a27f52844f71a1990dda85cc6bc4e834
otchereErnest <ernestowusu80@gmail.com>
2022-01-25 11:20:46 +0000

    added connection string-01

--- utils/db.js
+++ utils/db.js
@@ -13,7 +13,7 @@ class Db {
 			database: 'd6jrel113aen55',
 			port:5432,
 			connectionString:'postgres://dslxpggkjhwcvv:b1d1e5ea6f72eca0f6a4237e9b348c4a49015a43fb68ceaa029951767fbdbd1c@ec2-3-216-113-109.compute-1.amazonaws.com:5432/d6jrel113aen55',
-			ssl: true
+			ssl: { rejectUnauthorized: false }
 			//debug: false
 		});
 		this.connection.connect().then(() => console.log('connected to database'))
