20974824afe99fb8ebf261adb19d3dfdc553ecd1
otchereErnest <ernestowusu80@gmail.com>
2022-01-25 15:41:46 +0000

    added connection string-08

--- utils/db.js
+++ utils/db.js
@@ -9,10 +9,10 @@ class Db {
 			//connectionLimit: 100,
 			host: 'ec2-3-216-113-109.compute-1.amazonaws.com',
 			user: 'dslxpggkjhwcvv',
-			password: 'b1d1e5ea6f72eca0f6a4237e9b348c4a49015a43fb68ceaa029951767fbdbd1c',
-			database: 'd6jrel113aen55',
+			password: '372f576fd09c3b0e9e33c8b45d2542a0a6efa2bc7c902cedaa747a94bf94e3a8',
+			database: 'd2fu9p749184d7',
 			port:5432,
-			connectionString:'postgres://dslxpggkjhwcvv:b1d1e5ea6f72eca0f6a4237e9b348c4a49015a43fb68ceaa029951767fbdbd1c@ec2-3-216-113-109.compute-1.amazonaws.com:5432/d6jrel113aen55',
+			connectionString:'postgres://nxvwigwzratpff:372f576fd09c3b0e9e33c8b45d2542a0a6efa2bc7c902cedaa747a94bf94e3a8@ec2-52-73-149-159.compute-1.amazonaws.com:5432/d2fu9p749184d7',
 			ssl: { rejectUnauthorized: false }
 			//debug: false
 		});
